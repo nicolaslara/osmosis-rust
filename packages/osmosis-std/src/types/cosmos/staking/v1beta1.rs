1e849f71eac94365aaee44915d6506923e09e14d
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-09-07 16:37:49 +0700

    HistoricalInfo is temporary disabled due to unsupported tendermint proto codegen

--- packages/osmosis-std/src/types/cosmos/staking/v1beta1.rs
+++ packages/osmosis-std/src/types/cosmos/staking/v1beta1.rs
@@ -3,22 +3,26 @@ use osmosis_std_derive::CosmwasmExt;
 /// It is stored as part of staking module's state, which persists the `n` most
 /// recent HistoricalInfo
 /// (`n` is set by the staking module's `historical_entries` parameter).
-#[derive(
-    Clone,
-    PartialEq,
-    ::prost::Message,
-    serde::Serialize,
-    serde::Deserialize,
-    schemars::JsonSchema,
-    CosmwasmExt,
-)]
-#[proto_message(type_url = "/cosmos.staking.v1beta1.HistoricalInfo")]
-pub struct HistoricalInfo {
-    #[prost(message, optional, tag = "1")]
-    pub header: ::core::option::Option<tendermint_proto::types::Header>,
-    #[prost(message, repeated, tag = "2")]
-    pub valset: ::prost::alloc::vec::Vec<Validator>,
-}
+
+// temporary remove due to tendermint proto not yet supported
+// by code gen
+// #[derive(
+//     Clone,
+//     PartialEq,
+//     ::prost::Message,
+//     serde::Serialize,
+//     serde::Deserialize,
+//     schemars::JsonSchema,
+//     CosmwasmExt,
+// )]
+// #[proto_message(type_url = "/cosmos.staking.v1beta1.HistoricalInfo")]
+// pub struct HistoricalInfo {
+//     #[prost(message, optional, tag = "1")]
+//     pub header: ::core::option::Option<tendermint_proto::types::Header>,
+//     #[prost(message, repeated, tag = "2")]
+//     pub valset: ::prost::alloc::vec::Vec<Validator>,
+// }
+
 /// CommissionRates defines the initial commission rates to be used for creating
 /// a validator.
 #[derive(
